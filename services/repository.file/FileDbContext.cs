d33497c30a0e0bcac79b6e7a7444eed61e5a33b9
Fawei <50777393@qq.com>
2026-01-05 23:00:34 +0800

    file-service - add file repository

--- services/repository.file/FileDbContext.cs
+++ services/repository.file/FileDbContext.cs
@@ -15,12 +15,20 @@ namespace repository.file.Repositories
         {
             base.OnModelCreating(modelBuilder);
 
-            var builder = modelBuilder.Entity<UploadedItem>();
-            // Because GUID is not sequential by default, we set the primary key as non-clustered to optimize performance.
-            builder.HasKey(entity => entity.Id).IsClustered(false);
-            builder.Property(entity => entity.FileName).IsRequired().HasMaxLength(512);
-            builder.Property(entity => entity.FileHash).IsRequired().HasMaxLength(64);
-            builder.HasIndex(entity => new { entity.FileHash, entity.FileSize }).IsUnique();
+            modelBuilder.Entity<UploadedItem>(entity =>
+            {
+                // Because GUID is not sequential by default, we set the primary key as non-clustered to optimize performance.
+                entity.HasKey(entity => entity.Id).IsClustered(false);
+                entity.Property(entity => entity.FileName).IsRequired().HasMaxLength(512);
+                entity.Property(entity => entity.FileHash).IsRequired().HasMaxLength(64);
+                entity.HasIndex(entity => new { entity.FileHash, entity.FileSize }).IsUnique();
+
+                entity.Property(x => x.FileSize).IsRequired();
+                entity.Property(x => x.FileHash).IsRequired();
+                entity.Property(x => x.BackupUrl).IsRequired();
+                entity.Property(x => x.RemoteUrl).IsRequired();
+                entity.Property(x => x.CreateTime).IsRequired();
+            });
         }
     }
 }
---
+++ services/repository.file/Repositories/IFileRepository.cs
@@ -0,0 +1,9 @@
+﻿using repository.file.Repositories.Entities;
+
+namespace repository.file.Repositories
+{
+    public interface IFileRepository
+    {
+        ValueTask<UploadedItem?> FindFileAsync(string sha256Hash, long fileSize, CancellationToken ct = default);
+    }
+}
