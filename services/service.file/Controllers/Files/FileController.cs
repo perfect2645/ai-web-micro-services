a40d3cd09fbb852500da276d86b97277a714f090
Fawei <50777393@qq.com>
2026-01-07 23:46:05 +0800

    fileservice- add filecontroller

---
+++ services/service.file/Controllers/Files/FileController.cs
@@ -0,0 +1,35 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using repository.file.Repositories.Entities;
+using service.file.Services;
+using System.ComponentModel.DataAnnotations;
+using Utils.Json;
+
+namespace service.file.Controllers.Files
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion(1.0)]
+    public class FileController : ControllerBase
+    {
+
+        private readonly ILogger<FileController> _logger;
+        private readonly IFileUploadService _fileUploadService;
+
+        public FileController(ILogger<FileController> logger, IFileUploadService fileUploadService)
+        {
+            _logger = logger;
+            _fileUploadService = fileUploadService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse<UploadedItem?>>> Get([FromQuery, Required] long fileSize,
+            [FromQuery, Required] string sha256Hash)
+        {
+            var file = await _fileUploadService.GetUploadedItemAsync(fileSize, sha256Hash);
+            var result = ApiResponse<UploadedItem?>.Success(file);
+
+            return Ok(result);
+        }
+    }
+}
--- services/service.file/Services/IFileUploadService.cs
+++ services/service.file/Services/IFileUploadService.cs
@@ -1,7 +1,10 @@
-﻿namespace service.file.Services
+﻿using repository.file.Repositories.Entities;
+
+namespace service.file.Services
 {
     public interface IFileUploadService
     {
+        Task<UploadedItem?> GetUploadedItemAsync(long fileSize, string sha256Hash, CancellationToken cancellationToken = default);
         Task<string> UploadFileAsync(Stream fileStream, string fileName, string? description = null, CancellationToken cancellationToken = default);
     }
 }
