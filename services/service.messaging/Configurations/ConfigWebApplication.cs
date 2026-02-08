bbaae325ca8d1a135a4ab2eddb10f1d23adca647
Fawei <50777393@qq.com>
2026-02-08 16:22:37 +0800

    fileservice - code clean

--- services/service.messaging/Configurations/ConfigWebApplication.cs
+++ services/service.messaging/Configurations/ConfigWebApplication.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.FileProviders;
-using NetUtils.Aspnet.Configurations;
+﻿using NetUtils.Aspnet.Configurations;
 using service.messaging.Hubs;
 
 namespace service.messaging.Configurations
@@ -10,16 +9,6 @@ namespace service.messaging.Configurations
         {
             public void ConfigApplication()
             {
-                // enable static files folder (default wwwroot)
-                app.UseStaticFiles();
-
-                // static files for image output (React client access path: /img-output-remote)
-                app.UseStaticFiles(new StaticFileOptions
-                {
-                    FileProvider = new PhysicalFileProvider(@"F:\dev-storage\image-ai"),
-                    RequestPath = "/imgs-remote"
-                });
-
                 app.ConfigApp();
                 // Mapping SignalR Hub endpoint (React client connection address: /realTimeHub)
                 app.MapHub<SignalRHub>("/signalRHub");
